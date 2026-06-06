bec4de9fe1b2618f412ef1a9f421c75b7292a261
大原守禮 <shoulidayuan@gmail.com>
2026-06-06 11:49:28 +0900

    Update scanner-1.py

--- app/scanner-1.py
+++ app/scanner-1.py
@@ -1,11 +1,14 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+
 while True:
     ret,frame = cap.read()
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) == 27:
         break
-    cap.release()
+    
+cap.release()
+
 cv2.destroyAllWindows()
     
