379aa4cc33317b2b9cb7e7b6a63c6106eda276c2
大原守禮 <shoulidayuan@gmail.com>
2026-06-07 16:02:00 +0900

    Update scanner-1.py

--- app/scanner-1.py
+++ app/scanner-1.py
@@ -1,14 +1,27 @@
+#カメラ撮影
+
+
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    
     ret,frame = cap.read()
+    
+    if not ret:
+        break
+    
     cv2.imshow("Camera", frame)
-    if cv2.waitKey(1) == 27:
+    
+    key = cv2.waitKey(1)
+    
+    if key == ord("s"):
+        cv2.imwrite("document.jpg", frame)
+        
+    elif key == 27:
         break
     
 cap.release()
 
 cv2.destroyAllWindows()
-    
